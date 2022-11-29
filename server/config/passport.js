d71210f0844730b9a63581fe71a216280c120936
DanielMalede <danielmalede@gmail.com>
2022-11-30 01:56:37 +0200

    MERN template

---
+++ server/config/passport.js
@@ -0,0 +1,23 @@
+const keys = require('./keys')
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt
+const options = {
+    secretOrKey:keys.secretKey,
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+}
+module.exports=(passport)=>{
+// passport.use(
+//     new JwtStrategy(options,
+//     (jwt_payload,done)=>{
+//         //ADD HERE MODEL! .findById(jwt_payload.id)
+//         .then((user)=>{
+//             console.log("user found");
+//             return done(null,user)
+//         })
+//         .catch((error)=>{
+//             console.log(error);
+//             return done(error)
+//         })
+//     })
+// )
+}
