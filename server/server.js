fdbf541f6309b7f364342a82bd83f092ff136ea4
DanielMalede <danielmalede@gmail.com>
2022-11-30 23:53:27 +0200

    .

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ dotenv.config()
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 6000;
+const port = process.env.PORT;
 require('./DB')
 const path = require('path');
 const passport = require('passport');
