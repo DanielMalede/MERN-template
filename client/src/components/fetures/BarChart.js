cffc5b2cac40fe3ddc1b84d98803c348c3726224
DanielMalede <danielmalede@gmail.com>
2022-12-01 17:52:33 +0200

    .

---
+++ client/src/components/fetures/BarChart.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { chart as ChartJS , BarElement} from 'chart.js'
+import {Bar} from 'react-chartjs-2'
+export default function BarChart() {
+  return (
+    <div>BarChart</div>
+  )
+}
