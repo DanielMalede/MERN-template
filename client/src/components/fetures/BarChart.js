6a3f894855677ff856fb90f8b441f15111cafab8
DanielMalede <danielmalede@gmail.com>
2022-12-02 23:10:03 +0200

    .

--- client/src/components/fetures/BarChart.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { chart as ChartJS, BarElement } from "chart.js";
-import { Bar } from "react-chartjs-2";
-export default function BarChart() {
-  return <div>BarChart</div>;
-}
---
+++ client/src/components/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LineChart from '../../features/LineChart/LineChart'
+export default function Home() {
+  return (
+    <div>
+        <LineChart/>
+        <h1>nvisuvbsui</h1>
+    </div>
+  )
+}
